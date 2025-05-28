4535f16c0f2b12c607268bcae607b587f21d3cda
Shubham Patil <shubhamspatilnbr@gmail.com>
2025-05-28 17:54:10 +0530

    finish submussion controllers

--- backend/src/controllers/submission.controller.js
+++ backend/src/controllers/submission.controller.js
@@ -1,8 +1,69 @@
-const getAllSubmissions = async (req, res) => {};
+import { db } from "../libs/db";
 
-const getSubmissionsForProblem = async (req, res) => {};
+const getAllSubmissions = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const submissions = await db.submission.findMany({
+      where: {
+        userId,
+      },
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Submissions fetched successfully",
+      submissions,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Failed to fetch submissions",
+    });
+  }
+};
 
-const getAllSubmissionsCountForProblem = async (req, res) => {};
+const getSubmissionsForProblem = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { problemId } = req.params;
+    const submissions = await db.submission.findMany({
+      where: {
+        userId,
+        problemId,
+      },
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Submissions fetched successfully",
+      submissions,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Failed to fetch submissions for problem",
+    });
+  }
+};
+
+const getAllSubmissionsCountForProblem = async (req, res) => {
+  try {
+    const { problemId } = req.params;
+    const submissions = await db.submission.count({
+      where: {
+        problemId,
+      },
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Submissions fetched successfully",
+      count: submissions,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Failed to fetch submissions for problem",
+    });
+  }
+};
 
 export {
   getAllSubmissions,
